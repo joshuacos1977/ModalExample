3559147ca6bc731982f22e2fdc97219703f5bada
joshuacos1977 <coskunolcucu@hotmail.com>
2022-03-16 02:04:48 -0400

    [main]

--- src/OptionsMenu.jsx
+++ src/OptionsMenu.jsx
@@ -3,25 +3,7 @@ import Select from 'react-select';
 import axios from 'axios';
 import TestComponent from './TestComponent';
 import './App.css';
-const options = [
-  { id:1, name: "360 Behavioral Health HR Support",email:"360bhhrsupport@qs2500.com",group:"360 Behavioral Health" },
-  { id:2, name: "Albero HR Support",email:"AKAHRSupport@qs2500.com",group:"Albero - Retirement Plans"},
-  { id:3, name: "Alcoa HR Support",email:"alcoahrsupport@qs2500.com",group:"Alcoa - Other" },
-  { id:4, name: "CLC Contractors HR Support",email:"clccontractorshrsupport@qs2500.com",group:"Quantum" },
-  { id:5, name: "Garnet Ford HR Support",email:"GarnetFordHRSupport@QS2500.com",group:"Garnet Ford - Retirement Plan" },
-  { id:6, name: "Garnet VW HR Support",email:"GarnetVWHRSupport@QS2500.com",group:"Garnet VW - Retirement Plan" },
-  { id:7, name: "Hill Wallack HR Support",email:"hillwallackhrsupport@qs2500.com",group:"Hill Wallack - Other"},
-  { id:8, name: "Miller Brothers HR Support",email:"millerbrothershrsupport@qs2500.com",group:"Quantum" },
-  { id:9, name: "Quantum HR Support",email:"quantumhrsupport@qs2500.com",group:"Quantum - Other"},
-  { id:10, name: "Quantum Strategies",email:"QuantumHR@qs2500.com",group:"SERV - Retirement Plans" },
-  { id:11, name: "Retirement Plans",email:"retirementplans@qs2500.com",group:"Retirement Plans" },
-  { id:12, name: "SB Conrad HR Support",email:"sbconradhrsupport@qs2500.com",group:"Quantum" },
-  { id:13, name: "Sheltering Arms - Recruiting",email:"shelteringarmsrecruiting@qs2500.com",group:"Sheltering Arms - Recruiting" },
-  { id:14, name: "Sheltering Arms HR Support",email:"shelteringarmshrsupport@qs2500.com",group:"Sheltering Arms - Other" },
-  { id:15, name: "Stell HR Support",email:"StellHRSupport@QS2500.com", group:"Stell - Retirement Plan"},
-  
- 
-];
+
 
 class OptionsMenu extends Component {
   constructor(props){
